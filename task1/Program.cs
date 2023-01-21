8be976b3007c4a45b0a9655949475a3dd2a0f8d2
UBaronRU <ubaron@mail.ru>
2023-01-21 14:24:30 +0300

    Готово

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// Показать таблиц
+int Num1 = 0;
+int Num2 = 0;
+Console.Write("Введите число: ");
+Num1 = int.Parse(Console.ReadLine() ?? "0");
+for (int i = 1; i < Num1+1; i++)
+{
+    Num2=i*i;
+    Console.WriteLine("Квадрат числа "+ i +" -> "+Num2);
+}
