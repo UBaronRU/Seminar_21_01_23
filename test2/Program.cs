99cc977b42e4ec05e81caffb98227500d54a0922
UBaronRU <ubaron@mail.ru>
2023-01-21 14:38:26 +0300

    finish

--- test2/Program.cs
+++ test2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти сумму чисел от 1 до А
+// Показать таблиц
+int Num1 = 0;
+int Num2 = 0;
+Console.Write("Введите число: ");
+Num1 = int.Parse(Console.ReadLine() ?? "0");
+for (int i = 1; i <= Num1; i++)
+{
+    Num2=Num2+i;
+}
+Console.WriteLine("Сумма чисел от 1 до "+ Num1 +" -> "+Num2);
+// вариант 2
+
+Num2 = (( 1+ Num1)*Num1)/2;
+Console.WriteLine("Сумма чисел от 1 до "+ Num1 +" -> "+Num2);
