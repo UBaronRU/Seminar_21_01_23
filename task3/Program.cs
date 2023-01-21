be89f13a18c9f2ce9fd875584af9b9d9baf039c7
UBaronRU <ubaron@mail.ru>
2023-01-21 16:07:49 +0300

    now

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,7 +1,8 @@
 ﻿int Num1; // Основное число
-
 int Num_rzd; // Разрядность основного числа
 Num_rzd = 1;
+string [] array = {"разрядов","разряд","разряда","разряда","разряда","разрядов","разрядов","разрядов","разрядов","разрядов",};
+string okonchanie= "";
 Console.Write("Введите число  : ");
 Num1 = int.Parse(Console.ReadLine());
 // Определяем количество разрядов в числе
@@ -10,14 +11,8 @@ while (Num1 / Math.Pow(10,Num_rzd) > 1 & Num_rzd < 10)
 {
 Num_rzd = Num_rzd + 1; 
 }
-switch (Num_rzd)
-{
-   case 1:  {Console.WriteLine(" У введенного числа "+Num1+" -> " + Num_rzd + " разряд");break;}
-   case 2:  {Console.WriteLine(" У введенного числа "+Num1+" -> " + Num_rzd + " разряда");break;}
-   case 3:  {Console.WriteLine(" У введенного числа "+Num1+" -> " + Num_rzd + " разряда");break;}
-   case 4:  {Console.WriteLine(" У введенного числа "+Num1+" -> " + Num_rzd + " разряда");break;}
-   case 5:  {Console.WriteLine(" У введенного числа "+Num1+" -> " + Num_rzd + " разрядов");break;}
-   case 6:  {Console.WriteLine(" У введенного числа "+Num1+" -> " + Num_rzd + " разрядов");break;}
-   case 7:  {Console.WriteLine(" У введенного числа "+Num1+" -> " + Num_rzd + " разрядов");break;}
-   default: {Console.WriteLine(" У введенного числа "+Num1+" -> " + Num_rzd + " разряда");break;}
-}
+//switch (Num_rzd)
+//{
+//
+//}
+Console.WriteLine(" У введенного числа "+Num1+" -> " + Num_rzd + " разряда");
